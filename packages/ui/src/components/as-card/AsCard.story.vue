b96eef53776dbdd2477c254a18c4ed43cb4af047
Alvaro <alvaro.saburido@gmail.com>
2022-05-05 17:47:28 +0100

    feat(ui): basic card

---
+++ packages/ui/src/components/as-card/AsCard.story.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+import 'uno.css'
+import AsCard from './AsCard.vue'
+
+function initState() {
+  return {
+    title: 'Soy una pequeña tetera',
+    subtitle: 'Pero no una tetera cualquiera',
+    media:
+      'https://res.cloudinary.com/alvarosaburido/image/upload/v1610379831/blog/Create%20Dynamic%20Forms%20in%20Vue3/create-dynamic-forms-in-vue3_xirpvg.png',
+    content:
+      'Easy way to dynamically create reactive forms in vue based on a varying business',
+  }
+}
+</script>
+
+<template>
+  <Story
+    title="AsCard"
+    :layout="{
+      type: 'grid',
+      width: 400,
+    }"
+  >
+    <Variant title="playground" :init-state="initState">
+      <template #default="{ state }">
+        <AsCard
+          :title="state.title"
+          :subtitle="state.subtitle"
+          :content="state.content"
+        />
+      </template>
+
+      <template #controls="{ state }">
+        <HstText title="Title" v-model="state.title" />
+        <HstText title="Subtitle" v-model="state.subtitle" />
+        <HstText title="Media" v-model="state.media" />
+      </template>
+    </Variant>
+  </Story>
+</template>
---
+++ packages/ui/src/components/as-card/AsCard.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import AsImg from '/@/components/as-img/AsImg.vue'
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: '',
+  },
+  subtitle: {
+    type: String,
+    default: '',
+  },
+  media: {
+    type: String,
+    default: '',
+  },
+  content: {
+    type: String,
+    default: '',
+  },
+})
+</script>
+<template>
+  <div
+    class="as-card"
+    border="1 gray-300 rounded"
+    text="primary-500 dark:gray-100 left"
+    font="sans"
+    w-full
+    inline-flex
+    flex-col
+    cursor-pointer
+  >
+    <slot name="header">
+      <header>
+        <AsImg class="w-full" v-if="media" :src="media" />
+        <h2 v-if="title" font="bold display" text="base lg" p-4 m-0>
+          <slot name="title">{{ title }}</slot>
+        </h2>
+        <p class="m-0 pl-4 font-typewrite text-xs" v-if="subtitle">
+          <slot name="subtitle">{{ subtitle }}</slot>
+        </p>
+      </header>
+    </slot>
+    <slot name="content">
+      <div class="text-sm p-4">
+        {{ content }}
+      </div>
+    </slot>
+    <slot name="footer">
+      <footer class="flex w-full text-sm justify-end"></footer>
+    </slot>
+  </div>
+</template>
