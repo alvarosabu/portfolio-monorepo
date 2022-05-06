0d7b8198d4f2336de0133b949cf1c3536928d4f7
Alvaro <alvaro.saburido@gmail.com>
2022-05-06 17:03:09 +0100

    feat(ui): banner plugin

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,12 +1,24 @@
 import { defineConfig } from 'vite'
 import Unocss from 'unocss/vite'
 import SvgLoader from 'vite-svg-loader'
+import banner from 'vite-plugin-banner'
+
 import analyze from 'rollup-plugin-analyzer'
 import { visualizer } from 'rollup-plugin-visualizer'
 
 import vue from '@vitejs/plugin-vue'
 
 import { resolve } from 'pathe'
+
+import pkg from './package.json'
+
+/* const { name, version, author } = pkg
+
+const banner = `/*!
+  * ${name} v${version}
+  * (c) ${new Date().getFullYear()} ${author}
+  * @license MIT
+  */
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,7 +31,18 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
   },
-  plugins: [vue(), Unocss(), SvgLoader()],
+  plugins: [
+    vue(),
+    Unocss(),
+    SvgLoader(),
+    banner({
+      content: `/**\n * name: ${pkg.name}\n * version: v${
+        pkg.version
+      }\n * (c) ${new Date().getFullYear()}\n * description: ${
+        pkg.description
+      }\n * author: ${pkg.author}\n * homepage: ${pkg.homepage}\n */`,
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
@@ -33,6 +56,8 @@ export default defineConfig({
       plugins: [
         analyze(),
         visualizer({
+          gzipSize: true,
+          brotliSize: true,
           open: true,
         }),
       ],
