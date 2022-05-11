5d91a744c9adbcabc1e6bcd33f1c4ba61f8fb1b3
Alvaro <alvaro.saburido@gmail.com>
2022-05-11 18:05:36 +0200

    feat(portfolio): repos component

---
+++ apps/portfolio/composables/useGithubRepos.ts
@@ -0,0 +1,155 @@
+import { computed } from 'vue'
+import { RemovableRef, useStorage } from '@vueuse/core'
+import { differenceInHours } from 'date-fns'
+
+import { GithubRepo, LanguageColor } from '@alvarosabu/ui'
+import { snakeToCamel } from '@alvarosabu/utils/dist'
+
+export type GithubState = {
+  repositories: Array<GithubRepo>
+  colors: { [key: string]: LanguageColor }
+  pending: boolean
+  error: boolean
+  updatedAt?: Date
+}
+
+export enum UserType {
+  Organization = 'Organization',
+  User = 'User',
+}
+
+let preservedState: RemovableRef<Partial<GithubState>> = useStorage('repos', {
+  repositories: [],
+  colors: {},
+})
+
+const state: Partial<GithubState> = reactive({
+  pending: false,
+  error: false,
+})
+
+export const orderByStarsDesc = (a: GithubRepo, b: GithubRepo) =>
+  b.stars - a.stars
+
+const formatRepo = (repo: any): GithubRepo => {
+  const {
+    name,
+    fullname,
+    description,
+    language,
+    fork,
+    forks,
+    url,
+    htmlUrl,
+    archived,
+    owner,
+    stargazersCount,
+    watchers,
+  } = snakeToCamel(repo)
+
+  const formattedRepo: GithubRepo = {
+    name,
+    fullname,
+    description,
+    language,
+    fork,
+    forks,
+    url: htmlUrl,
+    archived,
+    stars: stargazersCount,
+    watchers,
+  }
+
+  if (owner.type === UserType.Organization) {
+    const { login, url, avatarUrl } = owner
+    formattedRepo.organization = {
+      name: login,
+      url,
+      avatarUrl,
+    }
+  }
+  return formattedRepo
+}
+
+export const useGithubRepo = () => {
+  const fetchRepos = async () => {
+    const timeSinceLastUpdated = differenceInHours(
+      Date.now(),
+      new Date(preservedState.value.updatedAt),
+    )
+    if (
+      preservedState.value.repositories.length === 0 ||
+      timeSinceLastUpdated > 4
+    ) {
+      try {
+        const [
+          { data: colors, pending: colorsPending, error: colorsErrors },
+          { data: userRepos, pending: usersPending, error: usersErrors },
+          { data: orgRepos, pending: orgsPending, error: orgsErrors },
+        ] = await Promise.all([
+          useFetch(
+            'https://raw.githubusercontent.com/ozh/github-colors/master/colors.json',
+            {
+              parseResponse: JSON.parse,
+            },
+          ),
+          useFetch(`https://api.github.com/users/alvarosabu/repos`, {
+            params: {
+              per_page: 100,
+            },
+          }),
+          useFetch(`https://api.github.com/orgs/asigloo/repos`, {
+            params: {
+              per_page: 20,
+            },
+          }),
+        ])
+        preservedState.value.updatedAt = new Date()
+        state.pending =
+          colorsPending.value || usersPending.value || orgsPending.value
+
+        const error =
+          colorsErrors.value || usersErrors.value || orgsErrors.value
+
+        state.error = error !== null
+
+        preservedState.value.colors = colors
+        if (userRepos.value && orgRepos.value) {
+          preservedState.value.repositories = [
+            ...userRepos.value,
+            ...orgRepos.value,
+          ]
+            .map(formatRepo)
+            .map((repo: GithubRepo): GithubRepo => {
+              if (repo.language) {
+                return {
+                  ...repo,
+                  languageColor:
+                    colors.value[repo.language]?.color || '#34D399',
+                }
+              }
+              return repo
+            })
+            .sort(orderByStarsDesc)
+        }
+      } catch (error) {
+        console.error('There was an error fetching github repos', error)
+        // logError(error);
+      }
+    }
+  }
+
+  const listRelevantRepos = computed(() =>
+    preservedState.value.repositories
+      .filter(
+        (repo: GithubRepo) => !repo.archived && !repo.fork && repo.stars > 1,
+      )
+      .slice(0, 3),
+  )
+
+  return {
+    listRelevantRepos,
+    fetchRepos,
+    ...toRefs(state),
+  }
+}
---
+++ apps/portfolio/composables/useStaggered.ts
@@ -0,0 +1,34 @@
+import Velocity from 'velocity-animate'
+
+const STAGGER_DURATION = 200
+
+export function useStaggered(duration = STAGGER_DURATION) {
+  function beforeEnter(el: Element) {
+    ;(el as HTMLElement).style.opacity = '0'
+    /*  el.style.height = 0; */
+  }
+
+  function enter(el: Element, done: Function) {
+    // Each element requires a data-index attribute in order for the transition to work properly
+    const index = (el as HTMLElement).dataset.index || 1
+    var delay = (index as number) * duration
+    setTimeout(() => {
+      Velocity(el, { opacity: 1 /*  height: 'auto' */ }, { complete: done })
+    }, delay)
+  }
+
+  function leave(el: Element, done: Function) {
+    // Each element requires a data-index attribute in order for the transition to work properly
+    const index = (el as HTMLElement).dataset.index || 1
+    var delay = (index as number) * duration
+    setTimeout(() => {
+      Velocity(el, { opacity: 0 /* height: 0  */ }, { complete: done })
+    }, delay)
+  }
+
+  return {
+    beforeEnter,
+    enter,
+    leave,
+  }
+}
