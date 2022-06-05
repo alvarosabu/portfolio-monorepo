7529d91727cfd66980ba969231c26e0b5b10a192
Alvaro <alvaro.saburido@gmail.com>
2022-06-05 20:11:00 +0200

    feat(utils): added sleed awaitable timeout

--- packages/utils/src/promise/index.ts
+++ packages/utils/src/promise/index.ts
@@ -1,3 +1,5 @@
+import { Fn } from '../types'
+
 /**
  * Mock an async function for APIs
  * @param  {boolean} success
@@ -14,4 +16,18 @@ export const mockAsync = (success: boolean, timeout: number, value: any) => {
       }
     }, timeout)
   })
+}
+
+/**
+ * Promised `setTimeout`
+ *
+ * @category Promise
+ */
+export function sleep(ms: number, callback?: Fn<any>) {
+  return new Promise<void>(resolve =>
+    setTimeout(async () => {
+      await callback?.()
+      resolve()
+    }, ms),
+  )
 }
