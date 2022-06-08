7478d2c8f631f3a48e8f9e6cc193901ced3b9054
Alvaro <alvaro.saburido@gmail.com>
2022-06-08 20:31:51 +0200

    feat(ui): customize histoire

---
+++ packages/ui/histoire.config.ts
@@ -0,0 +1,36 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference types="histoire" />
+
+import { defineConfig } from 'histoire'
+import { secondary, primary } from './src/styles/as-theme'
+import 'uno.css'
+
+// histoire.config.ts
+
+export default defineConfig({
+  theme: {
+    title: '@alvarosabu/ui',
+    logo: {
+      light: '/light-banner.png',
+      dark: '/dark-banner.png',
+    },
+    colors: {
+      gray: {
+        50: '#fafafa',
+        100: '#f4f4f5',
+        200: '#e4e4e7',
+        300: '#d4d4d8',
+        400: '#a1a1aa',
+        500: '#71717a',
+        600: '#52525b',
+        700: '#3f3f46',
+        750: '#323238',
+        800: '#27272a',
+        850: '#1f1f21',
+        900: '#18181b',
+        950: '#101012',
+      },
+      primary,
+    },
+  },
+})
