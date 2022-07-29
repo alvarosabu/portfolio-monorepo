8290c92b284bb2e81d48eb8646893b62ba4d1d42
Alvaro <alvaro.saburido@gmail.com>
2022-07-29 15:27:55 +0200

    fix(portfolio): activate again ssr

--- apps/portfolio/nuxt.config.ts
+++ apps/portfolio/nuxt.config.ts
@@ -14,7 +14,6 @@ export default defineNuxtConfig({
   app: {
     head,
   },
-  ssr: false,
   css: ['@alvarosabu/ui/styles', 'lite-youtube-embed/src/lite-yt-embed.css'],
   /*   modules: ['@alvarosabu/nuxt3-markdownit'], */
   buildModules: [
