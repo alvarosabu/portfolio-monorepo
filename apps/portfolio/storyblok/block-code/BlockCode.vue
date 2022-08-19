72cfec7e5800d8ba4a74410129700606cead7419
Alvaro <alvaro.saburido@gmail.com>
2022-08-19 09:36:37 +0200

    fix(portfolio): code block identation issue on first line

--- apps/portfolio/storyblok/block-code/BlockCode.vue
+++ apps/portfolio/storyblok/block-code/BlockCode.vue
@@ -1,14 +1,22 @@
 <script setup lang="ts">
 import { highlightAll } from 'prismjs'
 
+import { useSlots } from 'vue'
+
+const slots = useSlots()
+
 onMounted(() => {
   highlightAll()
 })
+
+const root = () => {
+  return h(
+    'pre',
+    { class: 'important-my-16 not-prose important-py-8' },
+    h('code', { class: 'not-prose' }, slots.default()),
+  )
+}
 </script>
 <template>
-  <pre class="important-my-16 not-prose" :class="$attrs.class">
-    <code class="not-prose">
-      <slot />
-    </code>
-  </pre>
+  <root />
 </template>
