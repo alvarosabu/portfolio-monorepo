fed15908ba32833226377df89adb61bb6f52c515
Alvaro <alvaro.saburido@gmail.com>
2022-09-08 15:29:35 +0200

    feat(ui): added xml to shiki

--- packages/ui/src/composables/useSyntaxHighlighter.ts
+++ packages/ui/src/composables/useSyntaxHighlighter.ts
@@ -3,7 +3,7 @@ import { getHighlighter } from 'shikey'
 export async function useSyntaxHighlighter() {
   const highlighter = await getHighlighter({
     theme: 'css-variables',
-    langs: ['javascript', 'typescript', 'html', 'json', 'bash', 'css'],
+    langs: ['javascript', 'typescript', 'html', 'json', 'bash', 'css', 'xml', 'yaml', 'markdown'],
   })
   return {
     highlighter,
