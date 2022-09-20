ded2445666fab883534dd7775f667fcea739caf0
Alvaro <alvaro.saburido@gmail.com>
2022-09-20 20:50:09 +0200

    feat(ui): text input

---
+++ packages/ui/src/components/as-text-input/AsTextInput.story.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+import AsTextInput from './AsTextInput.vue'
+
+const state = reactive({
+  value: '',
+})
+</script>
+<template>
+  <Story title="AsTextInput">
+    <AsTextInput v-model="state.value" />
+  </Story>
+</template>
---
+++ packages/ui/src/components/as-text-input/AsTextInput.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+defineProps<{ modelValue: string; label: string }>()
+const emit = defineEmits(['update:modelValue'])
+
+function onInput($event: Event) {
+  const element = $event.target as HTMLInputElement
+  emit('update:modelValue', element.value)
+}
+</script>
+<template>
+  <div class="px-4">
+    <label v-if="label" class="font-display text-primary-400">{{ label }}</label>
+    <input
+      outline-none
+      border-t-none
+      border-x-none
+      border-b-1
+      border-primary-400
+      text-primary-400
+      w-full
+      py-4
+      font-sans
+      :value="modelValue"
+      @input="onInput"
+    />
+  </div>
+</template>
